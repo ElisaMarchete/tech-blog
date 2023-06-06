372f28fb5e5daa437a3c14d2c93d70e75517c05d
Joao Marchete <elisamarchete@gmail.com>
2023-06-06 16:35:44 -0400

    extentions added

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sequelize = require("./config/connection");
-// const helpers = require('./utils/helpers');
-// const routes = require('./controllers');
+// const helpers = require("./utils/helpers");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,7 +25,7 @@ app.use(session(sess));
 
 // const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
+// app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
