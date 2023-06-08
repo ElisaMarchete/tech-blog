f69bbd7eea0e6b4cb9520c29bc67ec0fdb967ee2
Joao Marchete <elisamarchete@gmail.com>
2023-06-08 14:16:30 -0400

    login, signup and logout added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Posts } = require("../models");
 
+// http://localhost:3001/
 router.get("/", async (req, res) => {
   try {
     const postData = await Posts.findAll({
@@ -11,11 +12,34 @@ router.get("/", async (req, res) => {
     //post.get({ plain: true }) will contain plain JavaScript objects representing each post, instead of Sequelize model instances.
     console.log({ allPosts });
 
-    res.render("homepage", { allPosts });
+    res.render("homepage", { allPosts, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+// http://localhost:3001/login
+// Login route
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render("login");
+});
+
+// http://localhost:3001/signup
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render("signup");
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const Posts = require("./Posts");
+const User = require("./User");
 
 // Products belongsTo Category
 // Product.belongsTo(Category, {
 //     foreignKey: "category_id",
 //   });
 
-module.exports = { Posts };
+module.exports = { Posts, User };
