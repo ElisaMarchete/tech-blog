24db0219b60bd157b29d6fba36cc1eacba7753d3
Joao Marchete <elisamarchete@gmail.com>
2023-06-09 23:03:26 -0400

    When select hide posts with diff id's and past the post id to the comment

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,11 +1,27 @@
 const box = document.querySelector("#comment-box");
 const postsBoxes = document.querySelectorAll(".posts-box");
+// console.log(postsBoxes);
 
-// document.querySelectorALL(".posts-box");
-// for each to add listener to each post box
-
+// for each to add listener click to each post
 postsBoxes.forEach((post) => {
   post.addEventListener("click", function (e) {
+    // display the comment box when the post is clicked
     box.style = "display: block";
+    //split the post id information to get the id number
+    getPostId = post.id.split("-")[1];
+    // console.log(getPostId);
+
+    // hide the other posts when one is clicked
+    postsBoxes.forEach((post) => {
+      if (post.id.split("-")[1] !== getPostId) {
+        post.style = "display: none";
+      }
+    });
+
+    // add the post id to the comment id
+    box.setAttribute("comment-post-id", getPostId);
+    console.log(box);
   });
 });
+
+// hide the other posts
